c8331c67acfc73e1df19a161a1be47e8fd0c6605
rpi4gx <rpi4gx@gmail.com>
2021-10-26 18:09:40 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+
+## Introduction
+
+The following repository contains a practical example of making use of [Ephemeral Proxies API](https://rapidapi.com/rpi4gx/api/ephemeral-proxies) to concurrently hit a website from many different proxies.
+
+## Installation
+1. Pull repository
+```
+git clone git@github.com:rpi4gx/ephemeral-proxies-concurrency-example.git
+```
+2. Install modules
+```
+$ npm install
+```
+
+## Running
+
+Example: 
+```
+$ npx ts-node src/main.ts \
+  --x-rapidapi-key MY_RAPIDAPI_KEY \
+  --number-of-proxies 200 \
+  --target https://www.mywebsite.com
+
+Collecting 200 proxies ...
+Hiting https://www.mywebsite.com using 200 proxies with 200 different ips from 24 different countries ...
+Successfull requests against target: 198
+```
+
+## Requeriments
+ * A valid RapidAPI key, you can obtain one for free on [Rapid API](https://rapidapi.com)
+ * Optional: A subscription to a payment plan for [Ephemeral Proxies API](https://rapidapi.com/rpi4gx/api/ephemeral-proxies)
---
+++ src/main.ts
@@ -0,0 +1,92 @@
+const axios = require("axios").default;
+const httpsProxyAgent = require('https-proxy-agent');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const argv = yargs(hideBin(process.argv)).argv;
+interface V1ProxyVisibility {
+    ip: string,
+    country_iso: string
+}
+interface V1ProxyAPIResponse {
+    id: string,
+    host: string,
+    port: number,
+    visibility: V1ProxyVisibility,
+}
+interface V1APIResponse {
+    success: boolean,
+    proxy: V1ProxyAPIResponse
+}
+
+function usage() {
+    console.log(`
+    Required options:
+        --x-rapidapi-key <key> RapidAPI user's key
+        --number-of-proxies <number> Number of proxies to use for hitting the target
+        --target <http/s target> Example: http://www.mywebsite.com
+    `)
+}
+
+async function getProxies(n: number): Promise<V1ProxyAPIResponse[]> {
+    var options = {
+        method: 'GET',
+        url: 'https://ephemeral-proxies.p.rapidapi.com/v1/proxy',
+        headers: {
+            'x-rapidapi-host': 'ephemeral-proxies.p.rapidapi.com',
+            'x-rapidapi-key': argv.xRapidapiKey
+        }
+    };
+    let p = []
+    for (let i=0; i<n; i++) {
+        p.push(axios.request(options))
+    }
+    let responses = await Promise.allSettled(p)
+    let successfullResponses = responses.filter(r => {
+        if (r.status === 'fulfilled') {
+            if (r.value.data.success) return true;
+            console.warn(r.value.data)
+        } else if (r.status === 'rejected') {
+            console.warn(r)
+        }
+        return false
+    })
+    return successfullResponses.map((r:any) => r.value.data.proxy)
+}
+
+async function hitTarget(proxIp: string, proxyPort: number,  targetUrl:string) {
+    let agent = new httpsProxyAgent(`http://${proxIp}:${proxyPort}`);
+    let options = {
+        httpsAgent: agent,
+        method: 'GET',
+        url: targetUrl,
+        headers: {
+            'accept': 'application/json'
+        }
+    }
+    return axios.request(options)
+}
+
+(async () => {
+    if (!argv.xRapidapiKey || !argv.numberOfProxies || !argv.target) {
+        usage()
+        process.exit(-1)
+    }
+    try {
+        let target = argv.target
+        let nProxies = parseInt(argv.numberOfProxies)
+        console.log(`Collecting ${nProxies} proxies ...`)
+        let proxies: V1ProxyAPIResponse[] = await getProxies(nProxies)
+        if (proxies.length > 0) {
+            let uniqueProxiesIp = new Set(proxies.map(p => p.visibility.ip)).size
+            let uniqueCountries = new Set(proxies.map(p => p.visibility.country_iso)).size
+            console.log(`Hiting ${target} using ${proxies.length} proxies with ${uniqueProxiesIp} different ips from ${uniqueCountries} different countries ...`)
+            let results = await Promise.allSettled(proxies.map(p => hitTarget(p.host, p.port, target)))
+            let targetSuccess = 0
+            results.map((r: any) => { if (r.status == 'fulfilled') targetSuccess++ })
+            console.log(`Successfull requests against target: ${targetSuccess}`)
+        }
+    } catch (e) {
+        console.warn(e)
+        process.exit(-1)
+    }
+})()
